8352c757d2b2214d8e7d55439a69f444d1fbdd7a
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2026-03-09 19:00:58 -0400

    feat: zone automations (#34)
    * update zones
    
    * fix: zone translations
    
    * fix neutral zone bounds
    
    * track previous zone
    
    * feat: add trench and bump automations
    
    * update zones
    
    * update drive constants
    
    - slower max drive speed for right now
    - change trench speed multiplier to .75
    
    * more updates to drive modes
    
    * refactor: `autoRotate` instead of `automaticDrive`
    
    - now includes thresholds and `+` button for autoRotate to cancel
    
    * rename auto rotate autolog
    
    * auto extend when at trench
    
    * fix merge whoops
    
    ---------
    
    Signed-off-by: Fisher <72705185+fisherjacobc@users.noreply.github.com>
    Co-authored-by: Jacob Fisher <rjbrown124@comcast.net>

--- src/main/java/us/kilroyrobotics/RobotContainer.java
+++ src/main/java/us/kilroyrobotics/RobotContainer.java
@@ -13,23 +13,30 @@
 
 package us.kilroyrobotics;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Radians;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import us.kilroyrobotics.Constants.DriveConstants;
 import us.kilroyrobotics.Constants.IntakeConstants.ActuatorConstants;
 import us.kilroyrobotics.Constants.IntakeConstants.RollerConstants;
 import us.kilroyrobotics.Constants.VisionConstants;
@@ -42,6 +49,8 @@ import us.kilroyrobotics.subsystems.drive.GyroIOSim;
 import us.kilroyrobotics.subsystems.drive.ModuleIO;
 import us.kilroyrobotics.subsystems.drive.ModuleIOSim;
 import us.kilroyrobotics.subsystems.drive.ModuleIOTalonFX;
+import us.kilroyrobotics.subsystems.drive.Zone;
+import us.kilroyrobotics.subsystems.drive.Zone.ZoneType;
 import us.kilroyrobotics.subsystems.intake.Intake;
 import us.kilroyrobotics.subsystems.intake.IntakeEvent;
 import us.kilroyrobotics.subsystems.intake.IntakeState;
@@ -80,6 +89,10 @@ public class RobotContainer {
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
+
+  // Drive Mode
+  @AutoLogOutput(key = "Odometry/AutoRotateEnabled")
+  private boolean autoRotate = true;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -206,17 +219,17 @@ public class RobotContainer {
                 () -> new Rotation2d()));
 
     // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+    controller.x().onTrue(drive.runOnce(drive::stopWithX));
 
     // Reset gyro to 0° when B button is pressed
     controller
         .b()
         .onTrue(
-            Commands.runOnce(
+            drive
+                .runOnce(
                     () ->
                         drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
+                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())))
                 .ignoringDisable(true));
 
     controller.povDown().onTrue(intake.triggerEvent(IntakeEvent.EXTEND));
@@ -224,7 +237,7 @@ public class RobotContainer {
     controller
         .povRight()
         .onTrue(
-            Commands.runOnce(
+            intake.runOnce(
                 () -> {
                   if (intake.getCurrentState() == IntakeState.EXTENDED) {
                     CommandScheduler.getInstance()
@@ -233,8 +246,93 @@ public class RobotContainer {
                     CommandScheduler.getInstance()
                         .schedule(intake.triggerEvent(IntakeEvent.STOP_INTAKING));
                   }
-                },
-                intake));
+                }));
+
+    controller.rightStick().onTrue(Commands.runOnce(() -> autoRotate = !autoRotate));
+    Command cancelAutoRotate = Commands.runOnce(() -> autoRotate = false);
+    controller.button(8).onTrue(cancelAutoRotate);
+    controller
+        .axisMagnitudeGreaterThan(Axis.kRightX.value, DriveConstants.autoRotateCancelThreshold)
+        .or(
+            controller.axisMagnitudeGreaterThan(
+                Axis.kRightY.value, DriveConstants.autoRotateCancelThreshold))
+        .onTrue(cancelAutoRotate);
+
+    Trigger inAutoRotate = new Trigger(() -> autoRotate);
+
+    Command returnToDefaultDrive =
+        Commands.runOnce(
+            () -> {
+              drive.getDefaultCommand().cancel();
+
+              drive.setDefaultCommand(
+                  DriveCommands.joystickDrive(
+                      drive,
+                      () -> -controller.getLeftY(),
+                      () -> -controller.getLeftX(),
+                      () -> -controller.getRightX()));
+            });
+
+    inAutoRotate
+        .and(() -> drive.getZoneType() == ZoneType.TRENCH)
+        .onTrue(
+            Commands.parallel(
+                drive.runOnce(
+                    () -> {
+                      drive.getDefaultCommand().cancel();
+
+                      double currentRotationDeg = drive.getRotation().getDegrees();
+                      Rotation2d rotation =
+                          (currentRotationDeg <= 0 && currentRotationDeg >= -90)
+                                  || (currentRotationDeg >= 0 && currentRotationDeg <= 90)
+                              ? Rotation2d.kZero
+                              : Rotation2d.k180deg;
+
+                      drive.setDefaultCommand(
+                          DriveCommands.joystickDriveAtAngle(
+                              drive,
+                              () -> -controller.getLeftY() * DriveConstants.trenchSpeedMultiplier,
+                              () -> -controller.getLeftX() * DriveConstants.trenchSpeedMultiplier,
+                              () -> rotation));
+                    }),
+                Commands.runOnce(
+                    () -> {
+                      if (intake.getCurrentState().ordinal() < IntakeState.EXTENDED.ordinal()) {
+                        intake.triggerEvent(IntakeEvent.EXTEND);
+                      }
+                    })));
+
+    inAutoRotate
+        .and(() -> drive.getZoneType() == ZoneType.BUMP)
+        .onTrue(
+            drive.runOnce(
+                () -> {
+                  drive.getDefaultCommand().cancel();
+
+                  Rotation2d rotation;
+
+                  boolean wasInNeutralZone = drive.getPreviousZone() == Zone.NEUTRAL_ZONE;
+                  boolean inAllianceHalf = drive.getPose().getX() < (FlippingUtil.fieldSizeX / 2.0);
+
+                  if (DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red)
+                    inAllianceHalf = !inAllianceHalf;
+
+                  if ((wasInNeutralZone && inAllianceHalf)
+                      || (!wasInNeutralZone && !inAllianceHalf)) {
+                    rotation = new Rotation2d(Degrees.of(45));
+                  } else {
+                    rotation = new Rotation2d(Degrees.of(135));
+                  }
+
+                  drive.setDefaultCommand(
+                      DriveCommands.joystickDriveAtAngle(
+                          drive,
+                          () -> -controller.getLeftY(),
+                          () -> -controller.getLeftX(),
+                          () -> rotation));
+                }));
+
+    inAutoRotate.and(() -> drive.getZoneType() == ZoneType.NORMAL).onTrue(returnToDefaultDrive);
 
     streamdeck
         .button(1)
--- src/main/java/us/kilroyrobotics/subsystems/drive/Drive.java
+++ src/main/java/us/kilroyrobotics/subsystems/drive/Drive.java
@@ -60,7 +60,6 @@ import org.littletonrobotics.junction.Logger;
 import us.kilroyrobotics.Constants;
 import us.kilroyrobotics.Constants.Mode;
 import us.kilroyrobotics.generated.TunerConstants;
-import us.kilroyrobotics.subsystems.drive.Zone.ZoneType;
 import us.kilroyrobotics.util.LocalADStarAK;
 
 public class Drive extends SubsystemBase {
@@ -288,6 +287,13 @@ public class Drive extends SubsystemBase {
 
     // Update gyro alert
     gyroDisconnectedAlert.set(!gyroInputs.connected && Constants.currentMode != Mode.SIM);
+
+    Zone newZone = Zone.getZoneFromPose(getPose());
+
+    if (newZone != zone) {
+      previousZone = zone;
+      zone = newZone;
+    }
   }
 
   /**
@@ -400,22 +406,26 @@ public class Drive extends SubsystemBase {
     return poseEstimator.getEstimatedPosition();
   }
 
+  private Zone previousZone = Zone.UNKNOWN;
+
+  /** Returns the previous zone of the robot. */
+  @AutoLogOutput(key = "Odometry/PreviousZone")
+  public Zone getPreviousZone() {
+    return previousZone;
+  }
+
+  private Zone zone = Zone.getZoneFromPose(getPose());
+
   /** Returns the current zone of the robot. */
   @AutoLogOutput(key = "Odometry/Zone")
   public Zone getZone() {
-    return Zone.getZoneFromPose(getPose());
+    return zone;
   }
 
   /** Returns the current zone of the robot. */
   @AutoLogOutput(key = "Odometry/ZoneType")
   public Zone.ZoneType getZoneType() {
-    Zone zone = getZone();
-
-    if (zone == null) {
-      return ZoneType.UNKNOWN;
-    }
-
-    return zone.getType();
+    return getZone().getType();
   }
 
   /** Returns the current odometry rotation. */
